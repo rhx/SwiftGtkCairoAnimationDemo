498d025295db479d4f28323f03770b373ebd0eb6
Rene Hexel <r.hexel@griffith.edu.au>
2021-01-16 07:47:48 +1000

    Add new build system

--- Package.swift
+++ Package.swift
@@ -5,7 +5,8 @@ import PackageDescription
 let package = Package(
     name: "GtkCairoAnimationDemo",
     dependencies: [
-        .package(url: "https://github.com/rhx/SwiftGtk.git", .branch("master")),
+        .package(name: "gir2swift", url: "https://github.com/rhx/gir2swift.git", .branch("main")),
+        .package(url: "https://github.com/rhx/SwiftGtk.git", .branch("gtk3")),
     ],
     targets: [
         .target(name: "GtkCairoAnimationDemo", dependencies: ["Gtk"]),
